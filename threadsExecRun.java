f4fa4c3fa06f227f5492591906a4031a4ac9f593
EricksonKevin <cowcutta90@gmail.com>
2017-10-19 11:18:02 -0700

    Add files via upload

--- threadsExecRun.java
+++ threadsExecRun.java
@@ -1,76 +1,80 @@
-
+import static java.lang.System.exit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
  /////@author cowcutta//////
 
 /*
-///This application runs three threads simultaneously without the loss of 
-data using a synchronized function.  The sum of the three threads will be printed
-after all have run and compiled.
+///This application illustrates the process of running an executor function while handling 
+three threads in five thread pools
 */
 
 
-
- 
 public class threadsExecRun {
     
-    private int count = 0;
+  
     
-    ///avoids loss of counts while threads running////
-    public synchronized void increment() {
-      count++;  
-    }
-    
-    public static void main(String[] args) {
-       threadsExecRun sample = new threadsExecRun();
-       sample.doWork();
+    public static void main (String[]args) throws InterruptedException {
         
-    }
-    
-    public void doWork(){
-        Thread t1 = new Thread(new Runnable(){
-            
-            public void run(){
-               //// counts to 5,000 //// 
-                for(int i=0; i<5000; i++){
-                    ////runs synchronized function//////
-                    increment();
-                }
-            }
-        });
         
-        Thread t2 = new Thread(new Runnable(){
-            public void run(){
-                /// counts to 5,001 ////
-                for(int i=0; i<5001; i++){
-                    ////runs synchronized function//////
-                    increment();
-                }
-            }
-        });
+                //print intialization
+                //Wait 2 seconds
+                //Print running
+                //wait 2 seconds
+                //Print Closing              
+                //Wait 2 seconds
         
-        Thread t3 = new Thread(new Runnable(){
-            
-            public void run(){
-               //// counts to 5,002 //// 
-                for(int i=0; i<5002; i++){
-                    ////runs synchronized function//////
-                    increment();
-                }
-            }
-        });
-        ////runs the three threads////
-        t1.start();
-        t2.start();
-        t3.start();
-        ////adds the results of each thread////
-        try{
-            t1.join();
-            t2.join();
-            t3.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i=1; i<=15;i++) {
+            executorService.execute(new Runnable(){
+                
+                @Override
+                public void run() {
+                     
+                    try {
+                    System.out.println("Initializing");                   
+                    Thread.currentThread().sleep(2000);
+                    System.out.println("Running");
+                    Thread.currentThread().sleep(2000);
+                    System.out.println("Closing");
+                    Thread.currentThread().sleep(2000);
+                    }catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                
+            }});
         }
-        System.out.println("Count is: " + count);
     }
 }
+               
+               
+  
+
+
+     
+          
+    
+
+          
+                
+    
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
